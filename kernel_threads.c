3e2630758501fd4975b747654302ac417d974da3
user_name <malagoudakis@gmail.com>
2017-11-10 20:06:27 +0200

    Revert "Merge pull request #2 from ania7x/sav"
    This reverts commit d92de26436cf80456bb020e7e1058327ef9db109, reversing
    changes made to 509339111e9575e5dc4efd5b58522db1b2bc284b.

--- kernel_threads.c
+++ kernel_threads.c
@@ -9,8 +9,6 @@
   */
 Tid_t sys_CreateThread(Task task, int argl, void* args)
 {
-   TCB* t = (TCB*) allocate_thread(THREAD_SIZE);
-
 	return NOTHREAD;
 }
 
@@ -28,30 +26,7 @@ Tid_t sys_ThreadSelf()
 int sys_ThreadJoin(Tid_t tid, int* exitval)
 {
 	int ev;
-  TCB* localTCB;
-  PTCB* localPTCB;
-
-  localTCB = get_TCB(tid);
-  localPTCB = localTCB->owner_ptcb;
-
-
-
-  Mutex_lock();
-
-  pthread_join(tid, ev);
-
-  if (ev == ESRCH) 
-  {
-    perror("Given Thread with TID: %d was not found to be joined", tid);
-  }
-  else if (ev == EDEADLK)
-  {
-    perror("Given Thread with TID: %d couldn't be joined as it's the running Thread", tid);
-  }
-  else if (ev == EINVAL)
-  {
-    perror("Given Thread with TID: %d is not joinable", tid);
-  }
+  ev = pthread_join(tid, exitval);
   return -1;
 }
 
